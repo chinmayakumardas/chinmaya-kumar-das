8452490d230c45c81b7d2d2a981f2d0710491abe
chinmayakumardas <chinmayakumardas2000@gmail.com>
2024-07-12 22:51:35 +0530

    start

---
+++ frontend/src/context/ThemeContext.jsx
@@ -0,0 +1,16 @@
+import React, { createContext, useState } from 'react';
+
+// Create Context
+export const ThemeContext = createContext();
+
+// ThemeProvider component
+export const ThemeContextProvider = ({ children }) => {
+  const [theme, setTheme] = useState();
+
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
